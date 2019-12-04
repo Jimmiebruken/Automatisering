7f4e4677a4454f9e46d64e6de3dd2c39df0b924d
Jimmie Bruken <jimmie.bruken@student.kyh.se>
2019-12-05 00:03:06 +0100

    TrainAnnouncement update

--- mongoDBDemo/MongoCrud.cs
+++ mongoDBDemo/MongoCrud.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace mongoDBDemo
 {
+
+    // MongoCRUD sköter kontakten med DB, OBS! DB IP är satt till LAN! Public MONGODB IP är 78.67.178.206
     public class MongoCRUD
     {
-        private IMongoDatabase db;
+        public static IMongoDatabase db;
 
-        public MongoCRUD(string database)
+        public  MongoCRUD(string database)
         {
             var client = new MongoClient("mongodb://192.168.1.66:27017/admin");
             db = client.GetDatabase(database);
@@ -27,7 +29,7 @@ namespace mongoDBDemo
         {
 
             // skriv in vilken model och tabell som ska hämtas
-            var collection = db.GetCollection<TrafikverketModel>("Station");
+            var collection = db.GetCollection<TrafikverketTrainStationModel>("Station");
 
             // b är en instans av modellen som ska användas. jämförelse operatorer kan användas för sökningen (utöka med && and funktion möjligt???)
             // jämförelse sker mot "search" variablen som skickas in i funktionen
@@ -41,6 +43,28 @@ namespace mongoDBDemo
                 Console.WriteLine(dokument.AdvertisedLocationName);
             }
 
+            // istället för en foreachloop kan man t.ex välja att returnera värdet ut från funktionen
+
+
+        }
+        public static IList<string> FindStationSign(string CountyNo)
+        {
+
+         
+            var collection = db.GetCollection<TrafikverketTrainStationModel>("Station");
+
+            IList<string> signList = new List<string>();
+
+            var dokuments = collection.Find(b => b.CountyNo == CountyNo).ToListAsync().Result;
+
+            // exempel på retur av flera dokument
+            foreach (var dokument in dokuments)
+            {
+
+                signList.Add(dokument.LocationSignature);
+            }
+
+            return signList;
             // istället för en foreachloop kan man t.ex välja att returnera värdet ut från funktionen
 
 
